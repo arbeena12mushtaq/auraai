1c023b86aac4bb7fdd4721ffd03ec07c674fd180
ArbeenaMushtaq <arbeena12mushtaq@gmail.com>
2026-04-30 18:55:47 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Rate limiting
